ad46f0404c9d26a0a34aca55f163b294d1a65da5
alex <766f6964+github@gmail.com>
2025-06-06 16:11:29 +0200

    added server connection test

--- manifest-v2/src/options/Options.vue
+++ manifest-v2/src/options/Options.vue
@@ -16,8 +16,10 @@
           <th>{{ server.protocol }}{{ server.host }}:{{ server.port }}</th>
           <th>{{ server.username }}</th>
           <th>{{ server.password }}</th>
-          <th><button type="button" @click.prevent="editServerAtIndex(idx)">edit</button><button type="button"
-              @click.prevent="removeServerAtIndex(idx)">remove</button></th>
+          <th><button type="button" @click.prevent="editServerAtIndex(idx)" :disabled="loading"
+              title="update server settings">✎</button><button type="button" @click.prevent="removeServerAtIndex(idx)"
+              :disabled="loading" title="remove server settings">🗑️</button>
+          </th>
         </tr>
       </tbody>
     </table>
@@ -25,40 +27,47 @@
       <legend>Server settings</legend>
       <p class="form-row">
         <label for="name">Name:</label>
-        <input type="text" id="name" v-model.trim="newServer.name" placeholder="my server name" required />
+        <input type="text" id="name" v-model.trim="newServer.name" placeholder="my server name" required
+          :disabled="loading" />
       </p>
       <p class="form-row">
         <label for="host">Protocol:</label>
-        <select id="protocol" v-model="newServer.protocol">
+        <select id="protocol" v-model="newServer.protocol" :disabled="loading">
           <option value="http://" default>http</option>
           <option value="https://">https</option>
         </select>
       </p>
       <p class="form-row">
         <label for="host">Host:</label>
-        <input type="text" id="host" v-model.trim="newServer.host" placeholder="127.0.0.1" required />
+        <input type="text" id="host" v-model.trim="newServer.host" placeholder="127.0.0.1" required
+          :disabled="loading" />
       </p>
 
       <p class="form-row">
         <label for="port">Port:</label>
-        <input type="number" id="port" v-model.number="newServer.port" placeholder="8080" required />
+        <input type="number" id="port" v-model.number="newServer.port" placeholder="8080" required
+          :disabled="loading" />
       </p>
 
       <p class="form-row">
         <label for="username">Username:</label>
-        <input type="text" id="username" v-model.trim="newServer.username"
-          placeholder="leave empty if no auth required" />
+        <input type="text" id="username" v-model.trim="newServer.username" placeholder="leave empty if no auth required"
+          :disabled="loading" />
       </p>
 
       <p class="form-row">
         <label for="password">Password:</label>
-        <input type="text" id="password" v-model="newServer.password" placeholder="leave empty if no auth required" />
+        <input type="text" id="password" v-model="newServer.password" placeholder="leave empty if no auth required"
+          :disabled="loading" />
       </p>
 
       <div class="button-group">
-        <button id="test" :disabled="!isServerConnectionFilled" @click="testServerConnection">Test connection</button>
-        <button id="save" :disabled="!isServerConnectionFilled" @click="saveServerConfiguration">Save
-          configuration</button>
+        <button id="test" :disabled="loading || !isServerConnectionFilled" @click="testServerConnection"
+          :class="{ 'rotate-button': testing }"><span v-show="testing">⚙</span> Test
+          connection</button>
+        <button id="save" :disabled="loading || !isServerConnectionFilled" @click="saveServerConfiguration"
+          :class="{ 'rotate-button': saving }"><span v-show="saving">⚙</span>
+          Save configuration</button>
       </div>
     </fieldset>
     <p v-if="statusMessage">{{ statusMessage }}</p>
@@ -69,6 +78,8 @@
 
 import { ref, onMounted, computed } from 'vue'
 
+const testing = ref(false);
+const saving = ref(false);
 const servers = ref([]);
 const statusMessage = ref(null);
 const isServerConnectionTestedSucessfully = ref(false);
@@ -88,6 +99,10 @@ onMounted(() => {
   });
 });
 
+const loading = computed(() => {
+  return (testing.value || saving.value);
+});
+
 const isServerConnectionFilled = computed(() => {
   const { name, host, port, username, password } = newServer.value;
   const isUsernameAndPasswordValid = (username && password) || (!username && !password);
@@ -102,8 +117,53 @@ const isServerConnectionFilled = computed(() => {
 });
 
 const testServerConnection = () => {
-  isServerConnectionTestedSucessfully.value = true;
-  statusMessage.value = isServerConnectionTestedSucessfully.value ? "✅ Connection checked sucessfully" : "❌ Connection error";
+  isServerConnectionTestedSucessfully.value = false;
+  statusMessage.value = null;
+  const kodiUrl = `${newServer.value.protocol}${newServer.value.host}:${newServer.value.port}/jsonrpc`;
+  const payload = {
+    "method": "Application.GetProperties",
+    params: {
+      "properties": ["version"]
+    },
+    id: 1,
+    jsonrpc: "2.0",
+  };
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  if (newServer.value.username && newServer.value.password) {
+    headers["Authorization"] = "Basic " + btoa(`${newServer.value.username}:${newServer.value.password}`);
+  }
+  testing.value = true;
+  fetch(kodiUrl, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Error connecting Kodi: ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .then((data) => {
+      testing.value = false;
+      isServerConnectionTestedSucessfully.value = true;
+      console.debug("Kodi response:", data);
+      if (data.result && data.result.version) {
+        statusMessage.value = `✅ Connection checked sucessfully, remote kodi version: ${data.result.version.major || "0"}.${data.result.version.minor || "0"} (${data.result.version.revision || "revision"} / ${data.result.version.tag || "tag"})`;
+      } else {
+        console.error("Invalid kodi version response");
+        statusMessage.value = "❌ Connection error: Invalid kodi version response";
+      }
+    })
+    .catch((error) => {
+      testing.value = false;
+      console.error("Error connecting Kodi:", error);
+      statusMessage.value = `❌ Connection error: ${error}`;
+    });
 };
 
 const saveToStorage = () => {
@@ -120,6 +180,7 @@ const saveToStorage = () => {
 };
 
 const saveServerConfiguration = () => {
+  saving.value = true;
   if (!newServer.value.id) {
     servers.value.push({
       id: Date.now(),
@@ -138,6 +199,7 @@ const saveServerConfiguration = () => {
   saveToStorage();
   isServerConnectionTestedSucessfully.value = false;
   statusMessage.value = "✅ Server settings saved sucessfully";
+  saving.value = false;
 };
 
 
